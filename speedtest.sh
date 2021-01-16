3c3ecf4fdd657aed89475e7121e5d663c79556e5
nanda <nanda.bhikkhu@sasanarakkha.org>
2021-01-16 09:28:20 +0800

    source files added to webpage & script cleanup

--- speedtest.sh
+++ speedtest.sh
@@ -2,24 +2,40 @@
 
 SCRIPT=$(readlink -f "$0")
 DIR=$(dirname "$SCRIPT")
+HTML_ROOT=/var/www/html
+
+NETFLIX_LOG=netflix.csv
+SPEEDTEST_LOG=speedtest.csv
+LOGS="$NETFLIX_LOG $SPEEDTEST_LOG"
+
+NETFLIX_PLOT="netflix.png"
+SPEEDTEST_PLOT="speedtest.png"
+SPEEDTEST_PING_PLOT="speedtest-ping.png"
+PLOTS="$NETFLIX_PLOT $SPEEDTEST_PLOT $SPEEDTEST_PING_PLOT"
 
 start=$(date  '+%Y%m%d %H:%M')
 fastres=$(fast -u --single-line)
-echo "$start,$(echo $fastres | cut -d ' ' -f 1,2),$(echo $fastres | cut -d ' ' -f 3,4)" >> $DIR/netflix.csv
+echo "$start,$(echo $fastres | cut -d ' ' -f 1,2),$(echo $fastres | cut -d ' ' -f 3,4)" >> "$DIR/$NETFLIX_LOG"
 
 
-# umobile kuala lumpur
-KL=11557
-UNI=9593
+# speedtest servers
+KL=11557 # umobile KL
+UNI=9593 # university Kuala Terengganu
 RES=$(speedtest --server $KL --csv)
 if [ $? = 0 ]; then
 	echo "$RES" >> $DIR/speedtest.csv
 else
 	RES=$(speedtest --server $UNI --csv)
-	[ $? = 0 ] && echo "$RES" >> $DIR/speedtest.csv
+	[ $? = 0 ] && echo "$RES" >> "$DIR/$SPEEDTEST_LOG"
 fi
 
 cd $DIR
-gnuplot -p netflix.gnuplot && cp netflix.png /var/www/html/ && chgrp www-data /var/www/html/netflix.png
-gnuplot -p speedtest.gnuplot && cp speedtest.png /var/www/html/ && chgrp www-data /var/www/html/speedtest.png
-gnuplot -p speedtest-ping.gnuplot && cp speedtest-ping.png /var/www/html && chgrp www-data /var/www/html/speedtest-ping.png
+gnuplot -p netflix.gnuplot
+gnuplot -p speedtest.gnuplot 
+gnuplot -p speedtest-ping.gnuplot
+
+for f in $LOGS $PLOTS; do
+	cp "$DIR/$f" "$HTML_ROOT/$f"
+	chgrp www-data "$HTML_ROOT/$f"
+done
+
